8a5469d1198a6670720c023bc8ac00daaa5228e9
Graham Ollis <plicease@cpan.org>
2013-12-19 07:50:43 +0000

    Import of PLICEASE/Alien-bz2-0.07 from CPAN.
    gitpan-cpan-distribution: Alien-bz2
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         PLICEASE/Alien-bz2-0.07.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/Alien/bz2.pm
+++ lib/Alien/bz2.pm
@@ -7,7 +7,7 @@ use Text::ParseWords qw( shellwords );
 use File::Spec;
 
 # ABSTRACT: Build and make available libbz2
-our $VERSION = '0.06'; # VERSION
+our $VERSION = '0.07'; # VERSION
 
 
 sub _dir
@@ -59,7 +59,7 @@ Alien::bz2 - Build and make available libbz2
 
 =head1 VERSION
 
-version 0.06
+version 0.07
 
 =head1 SYNOPSIS
 
